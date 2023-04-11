302d465b687b3fcfbe85509a5c4a5c78f225ac79
Brady Rohrig <brady.rohrig@gmail.com>
2023-04-11 13:51:30 -0700

    feat: implemented functionality for customer creation endpoint

--- app/controllers/api/v1/customers_controller.rb
+++ app/controllers/api/v1/customers_controller.rb
@@ -1,6 +1,19 @@
 class Api::V1::CustomersController < ApplicationController
   def create
-    binding.pry
+    new_customer = Customer.create(customer_data)
+    render json: CustomerSerializer.new(new_customer)
+  end
+
+  private
+
+  def customer_data
+    params.permit(:first_name, 
+                  :last_name, 
+                  :address, 
+                  :email, 
+                  :city, 
+                  :zip_code, 
+                  :password)
   end
 
 end
