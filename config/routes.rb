e399ab596f930075f069bb47618c15715ece256d
Brady Rohrig <brady.rohrig@gmail.com>
2023-04-11 15:22:27 -0700

    feat: updated routes to have subscription creation endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
-      resources :customers, only: [:create, :update, :destroy]
+      resources :customers, only: [:create, :update, :destroy] do
+        resources :subscriptions, only: [:create]
+      end
     end
   end
 end
