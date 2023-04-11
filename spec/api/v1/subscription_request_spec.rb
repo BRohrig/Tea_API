e6ddc684daa0f7748a79db3b6076f0a3a91df2cd
Brady Rohrig <brady.rohrig@gmail.com>
2023-04-11 15:09:14 -0700

    test: wrote test for creation of a new subscription for a customer

---
+++ spec/api/v1/subscription_request_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.describe 'subscription endpoints' do
+  before(:all) do
+    @customer = create(:customer)
+  end
+
+  it 'has an endpoint to create a subscription for a given customer' do
+    subscription_data = { nickname: "I love this tea!",
+                          price: 20,
+                          status: "Active",
+                          frequency: "Monthly"}
+
+    post api_v1_customer_subscriptions_path(@customer.id), params: subscription_data
+
+    expect(response).to be_successful
+
+    subscription = JSON.parse(response.body, symbolize_names: true)[:data]
+
+    expect(subscription[:nickname]).to eq("I love this tea!")
+    expect(subscription[:price]).to eq(20)
+    expect(subscription[:status]).to eq("Active")
+    expect(subscription[:frequency]).to eq("Monthly")
+    @customer.subscriptions.last = new_subscription
+
+    expect(new_subscription.nickname).to eq("I love this tea!")
+    expect(new_subscription.price).to eq(20)
+    expect(new_subscription.status).to eq("Active")
+    expect(new_subscription.frequency).to eq("Monthly")
+  end
+end
