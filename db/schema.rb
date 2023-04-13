6b4fe794ec614bd1a7c793c3ceea496a876cfa8d
Brady Rohrig <brady.rohrig@gmail.com>
2023-04-13 08:53:11 -0700

    fix: added price to teas table

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_230_412_013_153) do
+ActiveRecord::Schema.define(version: 20_230_413_154_931) do
   # These are extensions that must be enabled in order to support this database
   enable_extension 'plpgsql'
 
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(version: 20_230_412_013_153) do
     t.float 'brew_time'
     t.datetime 'created_at', precision: 6, null: false
     t.datetime 'updated_at', precision: 6, null: false
+    t.float 'price'
   end
 
   add_foreign_key 'subscriptions', 'customers'
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -6,6 +7,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Tea.create({name: "This is a delcious tea",
-            variety: "Green Tea",
-            brew_time: 3.2})
+Tea.create({ name: 'This is a delcious tea',
+             variety: 'Green Tea',
+             brew_time: 3.2 })
